c4266dbc3d7f32c1a3d194f114328483a505cfcf
UTKARSH PRIYADARSHI <utkarshpriyadarshi5026@gmail.com>
2024-05-02 17:43:46 +0530

    228. Summary Ranges

---
+++ src/arrays/easy/228.java
@@ -0,0 +1,36 @@
+package arrays.easy;
+
+import java.util.*;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0)
+            return result;
+        int start = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int prev = nums[i - 1];
+            int curr = nums[i];
+
+            if (curr - prev == 1)
+                continue;
+            updateRange(prev, start, result);
+            start = curr;
+        }
+
+        updateRange(nums[nums.length - 1], start, result);
+        return result;
+
+    }
+
+    private void updateRange(int prev, int start, List<String> ranges) {
+        if (start == prev) {
+            ranges.add(String.valueOf(prev));
+            return;
+        }
+
+        String range = start + "->" + prev;
+        ranges.add(range);
+    }
+}
