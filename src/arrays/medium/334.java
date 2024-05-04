70b26718e37baffd64a0a2ee8b2897b3f3a396c4
UTKARSH PRIYADARSHI <utkarshpriyadarshi5026@gmail.com>
2024-05-05 00:52:17 +0530

    334. Increasing Triplet Subsequence

--- src/arrays/medium/334.java
+++ src/arrays/medium/334.java
@@ -6,19 +6,20 @@ import java.util.Arrays;
 // 334. Increasing Triplet Subsequence
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int[] triplets = new int[3];
-        Arrays.fill(triplets, Integer.MAX_VALUE);
-        triplets[0] = nums[0];
-
+        int[] smallestTwo = new int[2];
+        Arrays.fill(smallestTwo, Integer.MAX_VALUE);
+        smallestTwo[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            int curr = nums[i];
-            if (curr < triplets[0])
-                triplets[0] = curr;
-            if (curr > triplets[0] && curr < triplets[1])
-                triplets[1] = curr;
-            else if (curr > triplets[1])
+            int currentNum = nums[i];
+
+            if (currentNum < smallestTwo[0])
+                smallestTwo[0] = currentNum;
+            else if (currentNum > smallestTwo[0] && currentNum < smallestTwo[1])
+                smallestTwo[1] = currentNum;
+            else if (currentNum > smallestTwo[1])
                 return true;
         }
+
         return false;
     }
 }
