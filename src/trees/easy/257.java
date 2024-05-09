d4fc2cddd7792abc196dc4dc898158d832dd58a4
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-05-09 21:07:04 +0530

    257. Binary Tree Paths

---
+++ src/trees/easy/257.java
@@ -0,0 +1,54 @@
+package trees.easy;
+
+import java.util.*;
+
+import javax.print.DocFlavor.STRING;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// https://leetcode.com/problems/binary-tree-paths/description/
+// 257. Binary Tree Paths
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths = new ArrayList<>();
+
+        if (root == null)
+            return paths;
+        createPaths(root, String.valueOf(root.val), paths);
+        return paths;
+    }
+
+    private void createPaths(TreeNode root, String nodesSoFar, List<String> paths) {
+
+        if (root == null)
+            return;
+
+        if (root.left == null && root.right == null) {
+            paths.add(nodesSoFar);
+            return;
+        }
+
+        if (root.left != null)
+            createPaths(root.left, nodesSoFar + "->" + root.left.val, paths);
+
+        if (root.right != null)
+            createPaths(root.right, nodesSoFar + "->" + root.right.val, paths);
+    }
+}
