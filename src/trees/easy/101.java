27db0b66b150ec22bf8e3b74bc0d0804a4eb7567
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-05-11 00:59:41 +0530

    101. Symmetric Tree

---
+++ src/trees/easy/101.java
@@ -0,0 +1,44 @@
+package trees.easy;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// https://leetcode.com/problems/symmetric-tree/description/
+// 101. Symmetric Tree
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return check(root.left, root.right);
+    }
+
+    private boolean check(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return check(left.left, right.right) && check(left.right, right.left);
+    }
+}
