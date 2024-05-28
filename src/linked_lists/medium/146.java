ddac5af86a2f8d4aba131c2482189469c4b038cf
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-05-29 01:45:12 +0530

    146. LRU Cache

---
+++ src/linked_lists/medium/146.java
@@ -0,0 +1,70 @@
+package linked_lists.medium;
+
+import java.util.*;
+
+// https://leetcode.com/problems/lru-cache/description/
+// 146. LRU Cache
+class Node {
+    int key;
+    int value;
+    Node prev;
+    Node next;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+class LRUCache {
+    private final int capacity;
+    private final HashMap<Integer, Node> cache;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.head = new Node(0, 0); // Dummy head
+        this.tail = new Node(0, 0); // Dummy tail
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void add(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node node = cache.get(key);
+            remove(node);
+            add(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+        Node node = new Node(key, value);
+        add(node);
+        cache.put(key, node);
+        if (cache.size() > capacity) {
+            Node lru = tail.prev;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+    }
+
+}
