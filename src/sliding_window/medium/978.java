08eaa8e19630c6a97744f2df50ac534a4fc89405
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-06-04 14:59:22 +0530

    978. Longest Turbulent Subarray

---
+++ src/sliding_window/medium/978.java
@@ -0,0 +1,34 @@
+package sliding_window.medium;
+
+// https://leetcode.com/problems/longest-turbulent-subarray/description/
+// 978. Longest Turbulent Subarray
+class Solution {
+    public int maxTurbulenceSize(int[] arr) {
+        if (arr.length < 2) {
+            return arr.length;
+        }
+
+        int maxSize = 1;
+        int left = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            int prevCompare = Integer.compare(arr[i - 1], arr[i]);
+            if (prevCompare == 0) {
+                left = i;
+                continue;
+            } else if (i < arr.length - 1) {
+                int nextCompare = Integer.compare(arr[i], arr[i + 1]);
+
+                System.out.println(prevCompare + " " + nextCompare + " " + i);
+                if (nextCompare * prevCompare != -1) {
+                    maxSize = Math.max(maxSize, i - left + 1);
+                    left = i;
+                }
+            }
+
+        }
+
+        maxSize = Math.max(maxSize, arr.length - left);
+        return maxSize;
+    }
+}
