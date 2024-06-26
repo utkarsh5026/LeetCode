0f7d524afe13bdc94460d26311859dbb394e058a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-06-26 20:28:43 +0530

    297. Serialize and Deserialize Binary Tree

---
+++ src/trees/hard/297.java
@@ -0,0 +1,83 @@
+package trees.hard;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/description/
+// 297. Serialize and Deserialize Binary Tree
+class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                sb.append(node.val).append(",");
+                queue.add(node.left);
+                queue.add(node.right);
+            } else {
+                sb.append("null").append(",");
+            }
+        }
+
+        // Remove the last comma
+        sb.setLength(sb.length() - 1);
+        return sb.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data == null || data.isEmpty()) {
+            return null;
+        }
+
+        String[] values = data.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+        while (!queue.isEmpty() && i < values.length) {
+            TreeNode node = queue.poll();
+            if (!values[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(values[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (i < values.length && !values[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(values[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+}
