3d414078237e1e6e172efea4bfb7809b0048556a
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-02 15:59:11 +0530

    912. Sort an Array

---
+++ src/heaps/medium/912.java
@@ -0,0 +1,44 @@
+package heaps.medium;
+
+// https://leetcode.com/problems/sort-an-array/description/
+// 912.Sort an Array
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int n = nums.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(nums, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            int temp = nums[0];
+            nums[0] = nums[i];
+            nums[i] = temp;
+
+            heapify(nums, i, 0);
+        }
+
+        return nums;
+    }
+
+    private void heapify(int[] nums, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && nums[left] > nums[largest]) {
+            largest = left;
+        }
+
+        if (right < n && nums[right] > nums[largest]) {
+            largest = right;
+        }
+
+        if (largest != i) {
+            int swap = nums[i];
+            nums[i] = nums[largest];
+            nums[largest] = swap;
+            heapify(nums, n, largest);
+        }
+    }
+}
