98767820147741e858ebf62a23d12acdfad960b7
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-03 02:56:08 +0530

    633. Sum of Square Numbers

---
+++ src/two_pointers/medium/633.java
@@ -0,0 +1,21 @@
+package two_pointers.medium;
+
+// https://leetcode.com/problems/sum-of-square-numbers/description/
+// 633. Sum of Square Numbers
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long left = 0;
+        long right = (long) Math.sqrt(c);
+        while (left <= right) {
+            long cur = left * left + right * right;
+            if (cur < c) {
+                left++;
+            } else if (cur > c) {
+                right--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
