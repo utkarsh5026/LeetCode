b0dc6761640d3f512523855961e54b2d66312198
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-07 12:40:28 +0530

    1518. Water Bottles

---
+++ src/arrays/easy/1518.java
@@ -0,0 +1,16 @@
+package arrays.easy;
+
+// https://leetcode.com/problems/water-bottles/description
+// 1518. Water Bottles
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            int remainder = numBottles % numExchange;
+            numBottles /= numExchange;
+            ans += numBottles;
+            numBottles += remainder;
+        }
+        return ans;
+    }
+}
