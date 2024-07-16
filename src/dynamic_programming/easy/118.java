5bb28c990cea8714e2d46310f9bb5ebd991d56f3
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-16 21:02:48 +0530

    118. Pascal's Triangle

---
+++ src/dynamic_programming/easy/118.java
@@ -0,0 +1,31 @@
+package dynamic_programming.easy;
+
+import java.util.*;
+
+// https://leetcode.com/problems/pascals-triangle/description/
+// 118. Pascal's Triangle 
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 0)
+            return new ArrayList<>();
+        if (numRows == 1) {
+            List<List<Integer>> result = new ArrayList<>();
+            result.add(Arrays.asList(1));
+            return result;
+        }
+
+        List<List<Integer>> prevRows = generate(numRows - 1);
+        List<Integer> newRow = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            newRow.add(1);
+        }
+
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+
+        prevRows.add(newRow);
+        return prevRows;
+    }
+}
