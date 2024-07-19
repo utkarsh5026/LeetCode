55c577fdabb80e929490ae25d6c2857af5f73f50
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-19 18:46:21 +0530

    1380. Lucky Numbers in a Matrix

---
+++ src/arrays/easy/1380.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/// https://leetcode.com/problems/lucky-numbers-in-a-matrix/description
+// 1380. Lucky Numbers in a Matrix
+class Solution {
+    public static List<Integer> luckyNumbers(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        Set<Integer> minInRows = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            int min = matrix[i][0];
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                }
+            }
+            minInRows.add(min);
+        }
+        Set<Integer> maxInCols = new HashSet<>();
+        for (int j = 0; j < n; j++) {
+            int max = matrix[0][j];
+            for (int i = 1; i < m; i++) {
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                }
+            }
+            maxInCols.add(max);
+        }
+
+
+        List<Integer> luckyNumbers = new ArrayList<>();
+        for (Integer num : minInRows) {
+            if (maxInCols.contains(num)) {
+                luckyNumbers.add(num);
+            }
+        }
+
+        return luckyNumbers;
+    }
+}
