b3b673730ecc248dbf9796646a0d806e78574920
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2024-07-30 01:16:08 +0530

    1859. Sorting the Sentence
    Signed-off-by: Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>

---
+++ src/strings/easy/1859.java
@@ -0,0 +1,21 @@
+package strings.easy;
+
+// https://leetcode.com/problems/sorting-the-sentence/description/
+// 1859. Sorting the Sentence
+class Solution {
+    public String sortSentence(String s) {
+        String[] str = s.split(" ");
+        String[] res = new String[str.length];
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (String elem : str) {
+            i = (int) (elem.charAt(elem.length() - 1) - '0');
+            res[i - 1] = elem.substring(0, elem.length() - 1);
+        }
+
+        for (i = 0; i < res.length - 1; i++)
+            sb.append(res[i]).append(" ");
+        sb.append(res[i]);
+        return sb.toString();
+    }
+}
